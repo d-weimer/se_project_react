42d95f2164a1ad3ba4b3f51277a1708a8a36483e
d-weimer <dweimer19@gmail.com>
2026-04-17 14:04:39 +0900

    Started Header mark-up

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-button">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img className="header__avatar" src={avatar} alt="Terrence Tegegne" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
