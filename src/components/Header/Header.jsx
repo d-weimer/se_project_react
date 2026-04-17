11e4011055b5253341af34544a55fc1a0c3cdb8d
d-weimer <dweimer19@gmail.com>
2026-04-18 06:31:26 +0900

    Cleaned up Header mark-up

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,11 +7,11 @@ function Header() {
     <header className="header">
       <img className="header__logo" src={logo} />
       <p className="header__date-and-location">June 15, New York</p>
-      <button className="header__add-clothes-button">+ Add clothes</button>
-      <div className="header__user-container">
+      <div className="header__container">
+        <button className="header__add-clothes-button">+ Add clothes</button>
         <p className="header__username">Terrence Tegegne</p>
-        <img className="header__avatar" src={avatar} alt="Terrence Tegegne" />
       </div>
+      <img className="header__avatar" src={avatar} alt="Terrence Tegegne" />
     </header>
   );
 }
