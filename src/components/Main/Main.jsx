62371c13bed63571d2159ca0fe6eade230908ac1
d-weimer <dweimer19@gmail.com>
2026-04-18 05:41:04 +0900

    ItemCard mark-up and minor fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,7 @@ function Main({ weatherData }) {
       <WeatherCard />
       <section className="cards">
         <p className="cards__text">
-          Today is 75 &deg; F / You may want to wear:
+          Today is 75&deg; F / You may want to wear:
         </p>
         <ul className="cards__list">
           {defaultClothingItems
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -4,7 +4,7 @@ import daySunny from "../../assets/day-sunny.png";
 function WeatherCard() {
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">75 &deg; F</p>
+      <p className="weather-card__temp">75&deg;F</p>
       <img className="weather-card__image" src={daySunny} alt="sunny" />
     </section>
   );
